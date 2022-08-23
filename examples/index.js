048b04059e1d7be187918acffa7adeb8253e9611
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-08-23 17:53:30 +0800

    init proj

---
+++ examples/index.js
@@ -0,0 +1,11 @@
+// What is Plugin?
+
+// plugin template
+class SomePlugin {
+    apply(compiler) {
+        compiler.hooks.thisCompilation.tap('SomePlugin', (compilation) => {
+            compilation.hooks.optimizeChunkAssets.tapAsync('SomePlugin', () => {})
+        })
+    }
+}
+
