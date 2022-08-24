775a449a7d33b1428cc29f122014dccb439d4b8c
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-08-24 22:21:50 +0800

    add register logic

--- tapable/ourTapable/Hook.js
+++ tapable/ourTapable/Hook.js
@@ -17,8 +17,45 @@ class Hook {
         // 注册方法
         this.tap = this.tap;
     }
+    // 这是因为不同类型的 Hook 最终编译出的执行函数是不同的形式，
+    // 所以这里以一种抽象方法的方式将 compile 方法交给了子类进行实现。
     compile(options) {
         throw new Error('Abstract: this method should be overridden');
+    }
+
+  /**
+   * 
+   * @param {*} type 注册的类型 promise、async、sync
+   * @param {*} options 注册时传递的第一个参数对象
+   * @param {*} fn 注册时传入监听的事件函数
+   */
+    _tap(type, options, fn) {
+        if (typeof options === 'string') {
+            options = {
+                name: options.trim(),
+            };
+        } else if (typeof options !== 'object' || options === null) {
+            throw new Error('Invalid tap options!');
+        }
+        // object type
+        if (typeof options.name !== 'string' || options.name === '') {
+            throw new Error('Missing name for tap');
+        }
+        // 合并参数
+        Object.assign({ type, fn }, options);
+        // 基于合并后options去 insert fn
+        this._insert(options);
+    }
+    _insert(item) {
+        // // this._resetCompilation();
+        this.taps.push(item);
+    }
+    
+    tap(options, fn) {
+        // 这里额外多做了一层封装 是因为this._tap是一个通用方法
+        // 这里我们使用的是同步 所以第一参数表示类型传入 sync
+        // 如果是异步同理为sync、promise同理为 promise 这样就很好的区分了三种注册方式
+        this._tap('sync', options, fn);
     }
 }
 
