f8e3fd9001b35b8dca37e9a9fbfa71601a4456d9
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-08-25 14:29:20 +0800

    add compile function

--- tapable/ourTapable/SyncHook.js
+++ tapable/ourTapable/SyncHook.js
@@ -1,4 +1,17 @@
 const Hook = require('./Hook.js');
+const HookCodeFactory = require('./HookCodeFactory.js');
+
+class SyncHookCodeFactory extends HookCodeFactory {
+    
+    content({ onError, onDone, rethrowIfPossible }) {
+        return this.callTapsSeries({
+            onError: (i, error) => onError(error),
+            onDone,
+            rethrowIfPossible,
+        });
+    }
+}
+const factory = new SyncHookCodeFactory();
 
 const TAP_ASYNC = () => {
     throw new Error('tapAsync is not supported on SyncHook');
@@ -7,8 +20,13 @@ const TAP_PROMISE = () => {
     throw new Error('tapPromise is not supported on SyncHook');
 }
 
-function COMPILE() {
-    
+/**
+ * 调用栈this.call() -> CALL_DELEGATE -> this._createCall() -> this.compile() -> hook.COMPILE()
+ * @param {*} options
+ */
+function COMPILE(options) {
+    factory.setup(this, options);
+    return factory.create(options);
 }
 
 // basic SyncHook file
