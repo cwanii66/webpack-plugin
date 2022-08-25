c7e9e3087c66ae62d26e78875bd371510ed41318
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-08-25 14:55:21 +0800

    factory create logic

--- tapable/ourTapable/HookCodeFactory.js
+++ tapable/ourTapable/HookCodeFactory.js
@@ -7,11 +7,56 @@ class HookCodeFactory {
     }
     // 初始化参数
     setup(instance, options) {
-        instance._x = options.taps.maps(i => i.fn);
+        instance._x = options.taps.map(i => i.fn);
     }
     // 编译最终需要生成的函数
     create(options) {
+        this.init(options);
+        // 最终编译生成的方法fn
+        let fn;
+        switch (this.options.type) {
+            case 'sync':
+                fn = new Function(
+                    this.args(),
+                    '"use strict"; \n' +
+                    this.header() +
+                    this.contentWithInterceptors({
+                        onError: (err) => `throw ${err};\n`,
+                        onResult: (res) => `return ${res};\n`,
+                        resultReturns: true,
+                        onDone: () => '',
+                        rethrowIfPossible: true,
+                    })
+                );
+                break;
+            // 其他钩子类型
+            default:
+                break;
+        }
+        return fn;
+    }
 
+    args({ before, after }) {
+        
+    }
+    header() {
+
+    }
+    contentWithInterceptors() {
+
+    }
+
+    /**
+     * @param {{ type: "sync" | "promise" | "async", taps: Array<Tap>, interceptors: Array<Interceptor> }}
+     */
+    init(options) {
+        this.options = options;
+        // 保存初始化Hook时的参数
+        this._args = options.args.slice();
+    }
+    deinit() {
+        this.options = undefined;
+        this._args = undefined;
     }
 }
 
