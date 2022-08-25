d035bf24b91d93f04ae40e3a4ffe9248552a6351
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-08-25 14:57:20 +0800

    add args function

--- tapable/ourTapable/HookCodeFactory.js
+++ tapable/ourTapable/HookCodeFactory.js
@@ -37,7 +37,15 @@ class HookCodeFactory {
     }
 
     args({ before, after }) {
-        
+        let allArgs = this._args;
+        if (before) allArgs = [ before ].concat(allArgs);
+        if (after) allArgs = allArgs.concat(after);
+        if (allArgs.length === 0) {
+            return '';
+        } else {
+            return allArgs.join(', ');
+        }
+        // ...
     }
     header() {
 
